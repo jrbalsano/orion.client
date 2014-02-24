29a72fabe506e58e00e984a662181c63a9c39f23
Jonathan Balsano <jrbalsano@gmail.com>
2014-02-24 17:23:27 -0500

    Bug 427399 - Provide an Orion Node shell based on tty.js
    Moved support to allow shells to exist simultaneously
    
    Signed-off-by: Jonathan Balsano <jrbalsano@gmail.com>

--- modules/orionode/index.js
+++ modules/orionode/index.js
@@ -87,54 +87,11 @@ function startServer(options) {
 				root: '/node'
 			}))
 			.listen(options.port);
+
 		// Socket server
-		//var io = socketio.listen(app, { 'log level': 1 });
+    var io = socketio.listen(app, { 'log level': 1 });
 
-    // Terminal Socket
-    var termio = socketio.listen(app, { 'log level': 1 });
-
-    termio.sockets.on('connection', function(sock) {
-
-      var buff = [];
-      // Open Terminal Connection
-      var terminal = pty.fork(process.env.SHELL || 'sh', [], {
-        name: require('fs').existsSync('/usr/share/terminfo/x/xterm-256color')
-        ? 'xterm-256color'
-        : 'xterm',
-               cols: 80,
-               rows: 24,
-               cwd: process.env.HOME
-      });
-
-      terminal.on('data', function(data) {
-        return !sock
-        ? buff.push(data)
-        : sock.emit('data', data);
-      });
-
-      console.log(''
-        + 'Created shell h pty master/slave'
-        + ' pair (master: %d, pid: %d)',
-        terminal.fd, terminal.pid);
-
-      // Set up communication paths
-      sock.on('data', function(data) {
-        terminal.write(data);
-      });
-
-      sock.on('disconnect', function() {
-        terminal.destroy()
-        termsocket = null;
-      });
-
-      while (buff.length) {
-        sock.emit('data', buff.shift());
-      }
-    });
-
-
-	  //appSocket.install({io: io, appContext: appContext});
-		appSocket.install({io: termio, appContext: appContext});
+		appSocket.install({io: io, appContext: appContext});
 		app.on('error', handleError);
 		return app;
 	} catch (e) {
